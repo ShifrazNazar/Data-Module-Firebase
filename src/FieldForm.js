3d5c0d33ce923f5caa991588425a2cbb952aeec1
Shifraz Nazar <shifraznazar@gmail.com>
2023-04-25 23:01:54 +0800

    add Image and PDF upload functionality

--- src/FieldForm.js
+++ src/FieldForm.js
@@ -1,11 +1,17 @@
 import React, { useState, useEffect } from "react";
 import db from "./firebase";
 import * as XLSX from "xlsx";
+import firebase from "firebase/compat/app";
+import "firebase/compat/storage";
+
+const storage = firebase.storage();
 
 const FieldForm = ({ collectionId }) => {
   const [fieldName, setFieldName] = useState("");
   const [fieldValue, setFieldValue] = useState("");
   const [fieldData, setFieldData] = useState([]);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [pdfUrls, setPdfUrls] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -16,6 +22,8 @@ const FieldForm = ({ collectionId }) => {
           ...doc.data(),
         }));
         setFieldData(data);
+        const pdfs = data.filter((doc) => doc.pdfURL);
+        setPdfUrls(pdfs.map((doc) => doc.pdfURL));
       } catch (error) {
         console.log(error);
       }
@@ -28,7 +36,6 @@ const FieldForm = ({ collectionId }) => {
       alert("Field name and field value are required");
       return;
     }
-
     try {
       await db.collection(collectionId).add({ [fieldName]: fieldValue });
       // Update the state of the form component to display the new field
@@ -47,7 +54,6 @@ const FieldForm = ({ collectionId }) => {
   const handleDeleteField = async (idToDelete) => {
     try {
       await db.collection(collectionId).doc(idToDelete).delete();
-
       // Refetch the data from Firestore to update the table
       const snapshot = await db.collection(collectionId).get();
       const data = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
@@ -58,18 +64,94 @@ const FieldForm = ({ collectionId }) => {
   };
 
   const handleExportToExcel = () => {
-    const data = fieldData.map((field) => {
-      const key = Object.keys(field)[1];
-      const value = Object.values(field)[1];
-      return {
-        Field: key,
-        Answer: value,
-      };
-    });
+    const data = fieldData
+      .filter((field) => !field.pdfURL && !field.imageUrl)
+      .map((field) => {
+        const key = Object.keys(field)[1];
+        const value = Object.values(field)[1];
+        return {
+          Field: key,
+          Answer: value,
+        };
+      });
     const worksheet = XLSX.utils.json_to_sheet(data);
     const workbook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(workbook, worksheet, "Fields");
     XLSX.writeFile(workbook, "fields.xlsx");
+  };
+
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+  const handleImageUpload = async () => {
+    if (!selectedFile) {
+      alert("Please select a file");
+      return;
+    }
+    try {
+      const imageName = selectedFile.name;
+      // Create a storage reference
+      const storageRef = storage.ref().child(`${collectionId}/${Date.now()}`);
+
+      // Upload the file to storage
+      const snapshot = await storageRef.put(selectedFile);
+
+      // Get the download URL
+      const downloadURL = await snapshot.ref.getDownloadURL();
+
+      // Create a new document in the Firestore collection with the download URL
+      await db
+        .collection(collectionId)
+        .add({ imageUrl: downloadURL, imageName: imageName });
+
+      // Reset the selected file
+      setSelectedFile(null);
+
+      // Refetch the data from Firestore to update the table
+      const snapshot2 = await db.collection(collectionId).get();
+      const data = snapshot2.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+      setFieldData(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handlePDFUpload = async () => {
+    if (!selectedFile) {
+      alert("Please select a file");
+      return;
+    }
+    try {
+      const fileName = selectedFile.name;
+      // Create a storage reference
+      const storageRef = storage.ref().child(`${collectionId}/${Date.now()}`);
+
+      // Upload the file to storage
+      const snapshot = await storageRef.put(selectedFile);
+
+      // Get the download URL
+      const downloadURL = await snapshot.ref.getDownloadURL();
+
+      // Create a new document in the Firestore collection with the download URL
+      await db
+        .collection(collectionId)
+        .add({ pdfURL: downloadURL, fileURL: fileName });
+
+      setPdfUrls((prevUrls) => [
+        ...prevUrls,
+        { pdfURL: downloadURL, fileURL: fileName },
+      ]);
+
+      // Reset the selected file
+      setSelectedFile(null);
+
+      // Refetch the data from Firestore to update the table
+      const snapshot2 = await db.collection(collectionId).get();
+      const data = snapshot2.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+      setFieldData(data);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -115,36 +197,114 @@ const FieldForm = ({ collectionId }) => {
         <table className="border">
           <thead className="bg-gray-200">
             <tr>
-              {fieldData.map((field) => (
-                <th className="px-4 py-2" key={field.id}>
-                  {Object.keys(field)[1]}
-                  <button
-                    className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded focus:outline-none focus:shadow-outline ml-2"
-                    onClick={() => handleDeleteField(field.id)}
-                  >
-                    X
-                  </button>
-                </th>
-              ))}
+              {fieldData.map((field) => {
+                const keys = Object.keys(field);
+                if (keys.includes("pdfURL") || keys.includes("imageUrl")) {
+                  return null; // skip this field
+                }
+                return (
+                  <th className="px-4 py-2" key={field.id}>
+                    {keys[1]}
+                    <button
+                      className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded focus:outline-none focus:shadow-outline ml-2"
+                      onClick={() => handleDeleteField(field.id)}
+                    >
+                      X
+                    </button>
+                  </th>
+                );
+              })}
             </tr>
           </thead>
           <tbody>
             <tr>
-              {fieldData.map((field) => (
-                <td className="border px-4 py-2" key={field.id}>
-                  {Object.values(field)[1]}
-                </td>
-              ))}
+              {fieldData.map((field) => {
+                const keys = Object.keys(field);
+                if (keys.includes("pdfURL") || keys.includes("imageUrl")) {
+                  return null; // skip this field
+                }
+                return (
+                  <td className="border px-4 py-2" key={field.id}>
+                    {Object.values(field)[1]}
+                  </td>
+                );
+              })}
             </tr>
           </tbody>
         </table>
       </div>
       <button
-        className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mb-3"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mb-3"
         onClick={handleExportToExcel}
       >
-        Export as Excel
+        Export to Excel
       </button>
+
+      {/* PDF upload */}
+      <div className="mb-4">
+        <label
+          className="block text-gray-700 font-bold mb-2"
+          htmlFor="fieldValue"
+        >
+          upload PDF
+        </label>
+        <input
+          className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline "
+          type="file"
+          onChange={handleFileChange}
+        />
+        <button>
+          <label
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mb-3"
+            onClick={handlePDFUpload}
+          >
+            Upload
+          </label>
+        </button>
+        <div>
+          {pdfUrls.map((pdf, index) => (
+            <div key={index}>
+              <a href={pdf.pdfURL} target="_blank" rel="noopener noreferrer">
+                {pdf.fileURL}
+              </a>
+            </div>
+          ))}
+        </div>
+
+        {/* Image upload */}
+      </div>
+      <div className="mb-4">
+        <label
+          className="block text-gray-700 font-bold mb-2"
+          htmlFor="fieldValue"
+        >
+          Upload Image
+        </label>
+        <input
+          className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline "
+          type="file"
+          onChange={handleFileChange}
+        />
+        <button>
+          <label
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mb-3"
+            onClick={handleImageUpload}
+          >
+            Upload
+          </label>
+        </button>
+        <div className="flex flex-wrap">
+          {fieldData
+            .filter((field) => field.hasOwnProperty("imageUrl"))
+            .map((field) => (
+              <div className="w-1/3 p-2" key={field.id}>
+                <div className="bg-white rounded shadow">
+                  <img className="w-full" src={field.imageUrl} alt="uploaded" />
+                </div>
+              </div>
+            ))}
+        </div>
+      </div>
     </div>
   );
 };
